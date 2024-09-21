56436a27f798fbf293fb14679ba22290dba165a8
nh3la <kyomaB777@gmail.com>
2024-09-21 12:49:47 -0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/flashcards2/" : "/",
   transpileDependencies: true
 })
